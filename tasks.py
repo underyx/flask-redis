a103c8ea81893a021a779cbf4dd0dcaf0fe4a24c
Rhys Elsmore <me@rhys.io>
2013-07-05 23:45:57 +1000

    Added in Taskfile for Testing

---
+++ tasks.py
@@ -0,0 +1,21 @@
+from invoke import run, task
+
+
+@task
+def test():
+    run('py.test test_flask_redis.py', pty=True)
+
+
+@task
+def coverage():
+    run('py.test --cov=flask_redis test_flask_redis.py', pty=True)
+
+
+@task
+def pep8():
+    run('py.test --pep8 test_flask_redis.py', pty=True)
+
+
+@task
+def full():
+    run('py.test --pep8 --cov=flask_redis test_flask_redis.py', pty=True)
